5ac42a8ac737a0ad5ca7c2debf1d4431106ac955
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-01-17 15:30:44 +0000

    passes test 10 and 11

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -31,3 +31,12 @@ def pets_by_breed(pet_shop, required_breed)
   end
   return found_pets
 end
+
+def find_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      return pet
+    end
+  end
+  return nil
+end
